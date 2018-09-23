edde947ce4cd9ead9e60817783efccb480040877
Song <songnan17@qq.com>
2018-09-23 22:37:27 +0800

    add my code

--- main/main.js
+++ main/main.js
@@ -1,18 +1,49 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.length()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+     this.input = input;// Write your code here
   }
 
   minimum() {
-    // Write your code here
+    var min = this.input[0];
+	for (var i = 1;i < this.input.length;i++) {
+		if (this.input[i] < min){
+			min = this.input[i];
+		}
+	}
+	return min;// Write your code here
   }
 
-  // Write your code here
+  maximum() {
+    var max = this.input[0];
+	for (var i = 1;i < this.input.length;i++) {
+		if (this.input[i] > max){
+			max = this.input[i];
+		}
+	}
+	return max;// Write your code here
+  }
+  
+  length() {
+	  var i = this.input.length;
+	  return i;
+  }
+  
+  average() {
+	  var s = 0;
+	  for (var i = 0;i < this.input.length;i++) {
+		  s = s + this.input[i];
+	  }
+	  var x = this.input.length;
+	  var ave = s/x;
+	  ave = ave.toFixed(2);
+	  return ave;
+  }
 }
